d09f1c22aec5dbda8294828fa8e03a59920a792c
zyj <617242499@qq.com>
2016-03-22 13:06:47 +0800

    121

--- Lsmsms.php
+++ Lsmsms.php
@@ -5,7 +5,7 @@
 	use Yii;
 	use yii\base\InvalidConfigException;
 	/**
-	 * 	云片短信平台
+	 * 	luosimao短信平台
 	 * 	
 	 * 	@author zyj
 	 */
@@ -32,14 +32,21 @@
 				'mobile'=>$mobile,
 				'message'=>$content
 			];
-			$user_pwd = 'api-key'.$this->key;
+			$user_pwd = 'api:key-'.$this->key;
 			$res = $this->LsmCurl($user_pwd,$data);
-			var_dump(json_decode($res));
-			exit;
+			$result = json_decode($res);
+			if($result->error!=0) {
+				$this->message =  $result->statusMsg;
+				$this->state = false;
+				//TODO 添加错误处理逻辑
+			}else{
+				$this->state = true;
+				$this->message = '发送成功';
+			}
+			return $this->state;
 		}
 
-
-		public function LsmCurl($user_pwd,$data){
+		protected function LsmCurl($user_pwd,$data){
 
 			$ch = curl_init();
 			curl_setopt($ch, CURLOPT_URL, $this->url);
